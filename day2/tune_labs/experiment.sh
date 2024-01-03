60d473de86393e4144d4687a8da4ee4cdb974f38
Martin Aegidius <gygridd@gmail.com>
2024-01-03 14:18:11 +0100

    updated experiment

--- day2/tune_labs/experiment.sh
+++ day2/tune_labs/experiment.sh
@@ -5,10 +5,10 @@
 #BSUB -R "select[model == XeonE5_2650v4]"
 #BSUB -W 01:00
 #BSUB -u gygridd@gmail.com
-#BSUB -o SOAvsAOS.out
+#BSUB -o /zhome/ba/f/147212/hpc/day2/tune_labs/logs/SOAvsAOS.out
 
 
-for nparticles in {2000..3000000..40000}
+for nparticles in {2000..3000000..4000}
 do 
 	/zhome/ba/f/147212/hpc/day2/tune_labs/soa.gcc 1000 $nparticles
 	/zhome/ba/f/147212/hpc/day2/tune_labs/aos.gcc 1000 $nparticles
